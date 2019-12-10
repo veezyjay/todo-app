51228395d7810967fb9a47ce8df1dac83287bbb8
Victor Bassey <viktorbasi545@gmail.com>
2019-12-10 17:10:24 +0100

    Create task entity
    Closes #1

---
+++ src/main/java/com/veezy/todoapp/model/Status.java
@@ -0,0 +1,5 @@
+package com.veezy.todoapp.model;
+
+public enum Status {
+    pending, in_progress, done
+}
---
+++ src/main/java/com/veezy/todoapp/model/Task.java
@@ -0,0 +1,82 @@
+package com.veezy.todoapp.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private Integer id;
+    private String title;
+    private String description;
+    private Status taskStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime completedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(Status taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", taskStatus=" + taskStatus +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", completedAt=" + completedAt +
+                '}';
+    }
+}
