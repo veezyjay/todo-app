8c28fa519b89a0aed8b348141fa453c32d53dcd9
Victor Bassey <viktorbasi545@gmail.com>
2019-12-10 17:25:39 +0100

    Link app to H2 database
    Closes #3

--- src/main/java/com/veezy/todoapp/model/Task.java
+++ src/main/java/com/veezy/todoapp/model/Task.java
@@ -1,14 +1,33 @@
 package com.veezy.todoapp.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "task_status")
     private Status taskStatus;
+
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "completed_at")
     private LocalDateTime completedAt;
 
     public Integer getId() {
