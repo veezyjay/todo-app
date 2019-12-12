7662b3c247c7e8a557ece6697219300beea9d9c3
Victor Bassey <viktorbasi545@gmail.com>
2019-12-12 12:20:13 +0100

    Add project lombok and refactor

--- src/main/java/com/veezy/todoapp/model/Task.java
+++ src/main/java/com/veezy/todoapp/model/Task.java
@@ -1,5 +1,6 @@
 package com.veezy.todoapp.model;
 
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,6 +9,12 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Task {
 
     @Id
@@ -35,73 +42,4 @@ public class Task {
 
     @Column(name = "completed_at")
     private LocalDateTime completedAt;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Status getTaskStatus() {
-        return taskStatus;
-    }
-
-    public void setTaskStatus(Status taskStatus) {
-        this.taskStatus = taskStatus;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public LocalDateTime getCompletedAt() {
-        return completedAt;
-    }
-
-    public void setCompletedAt(LocalDateTime completedAt) {
-        this.completedAt = completedAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", taskStatus=" + taskStatus +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                ", completedAt=" + completedAt +
-                '}';
-    }
 }
