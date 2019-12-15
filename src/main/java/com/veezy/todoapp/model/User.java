10e88b3324df5d1697dffa961543cf0fdd397f04
Victor Bassey <viktorbasi545@gmail.com>
2019-12-15 19:47:41 +0100

    Create user model

---
+++ src/main/java/com/veezy/todoapp/model/User.java
@@ -0,0 +1,64 @@
+package com.veezy.todoapp.model;
+
+import java.util.List;
+
+public class User {
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private List<Task> tasks;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
