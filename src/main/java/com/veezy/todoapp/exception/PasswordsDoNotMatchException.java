d0483410da6893cd4300e322894d8456f450486e
Victor Bassey <viktorbasi545@gmail.com>
2020-01-04 15:53:50 +0100

    Add exception for passwords that do not match

---
+++ src/main/java/com/veezy/todoapp/exception/PasswordsDoNotMatchException.java
@@ -0,0 +1,17 @@
+package com.veezy.todoapp.exception;
+
+public class PasswordsDoNotMatchException extends RuntimeException {
+    public PasswordsDoNotMatchException() {}
+
+    public PasswordsDoNotMatchException(String message) {
+        super(message);
+    }
+
+    public PasswordsDoNotMatchException(Throwable cause) {
+        super(cause);
+    }
+
+    public PasswordsDoNotMatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
