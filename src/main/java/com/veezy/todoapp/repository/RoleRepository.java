728b22d45f1d6f95319f5e0617921c0809cb3047
Victor Bassey <viktorbasi545@gmail.com>
2020-01-04 15:32:05 +0100

    Add repository for user roles

---
+++ src/main/java/com/veezy/todoapp/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.veezy.todoapp.repository;
+
+import com.veezy.todoapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
