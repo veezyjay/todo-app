d85cab5db7d56b2abf32fcaef66bf0dd5fd709ee
Victor Bassey <viktorbasi545@gmail.com>
2020-01-04 15:17:32 +0100

    Configure spring security

---
+++ src/main/java/com/veezy/todoapp/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.veezy.todoapp.config;
+
+import com.veezy.todoapp.filter.JwtRequestFilter;
+import com.veezy.todoapp.service.TodoUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private TodoUserDetailsService todoUserDetailsService;
+    private JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(TodoUserDetailsService todoUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.todoUserDetailsService = todoUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(todoUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/registered-users").hasRole("ADMIN")
+                .antMatchers("/users/hello-user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
