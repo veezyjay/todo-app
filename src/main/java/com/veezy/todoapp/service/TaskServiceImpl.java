eb7f9abcdb776b26a2a3d365e3ee955fef3600ee
Victor Bassey <viktorbasi545@gmail.com>
2020-01-04 15:46:41 +0100

    Refactor task service to use currently logged in user

--- src/main/java/com/veezy/todoapp/service/TaskServiceImpl.java
+++ src/main/java/com/veezy/todoapp/service/TaskServiceImpl.java
@@ -1,64 +1,81 @@
 package com.veezy.todoapp.service;
 
+import com.veezy.todoapp.authentication.AuthenticationFacade;
+import com.veezy.todoapp.exception.ResourceNotFoundException;
 import com.veezy.todoapp.model.Status;
 import com.veezy.todoapp.model.Task;
+import com.veezy.todoapp.model.User;
 import com.veezy.todoapp.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
+    private UserService userService;
     private TaskRepository taskRepository;
+    private AuthenticationFacade authenticationFacade;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, UserService userService,
+                           AuthenticationFacade authenticationFacade) {
         this.taskRepository = taskRepository;
+        this.userService = userService;
+        this.authenticationFacade = authenticationFacade;
     }
 
     @Override
     public Task addTask(Task theTask) {
+        User user = userService.getUser(authenticationFacade.getId());
+        theTask.setTaskStatus(Status.pending);
+        theTask.setTaskCreator(user);
         return taskRepository.save(theTask);
     }
 
     @Override
     public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+        User user = userService.getUser(authenticationFacade.getId());
+        return user.getTasks();
     }
 
     @Override
     public Task getTask(Integer taskId) {
-        Optional<Task> theTask = taskRepository.findById(taskId);
-        Task task = null;
-        if (theTask.isPresent()) {
-            task = theTask.get();
-        }
-        return task;
+        return taskRepository.findByIdAndTaskCreatorId(taskId, authenticationFacade.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
     }
 
     @Override
     public String deleteTask(Integer taskId) {
-        taskRepository.deleteById(taskId);
+        Optional<Task> taskOptional = taskRepository.findByIdAndTaskCreatorId(taskId, authenticationFacade.getId());
+        if (taskOptional.isEmpty()) {
+            throw new ResourceNotFoundException("Task not found");
+        } else {
+            taskRepository.delete(taskOptional.get());
+        }
         return "Successfully deleted task with id - " + taskId;
     }
 
     @Override
-    public Task updateTask(Task task) {
-        Integer taskId = task.getId();
-        if (taskId != null) {
-            Optional<Task> taskOptional = taskRepository.findById(taskId);
-            if (taskOptional.isPresent()) {
-                Task theTask = taskOptional.get();
-                task.setCreatedAt(theTask.getCreatedAt());
+    public Task updateTask(Task task, Integer taskId) {
+        Optional<Task> taskOptional = taskRepository.findByIdAndTaskCreatorId(taskId, authenticationFacade.getId());
+        if (taskOptional.isPresent()) {
+            Task theTask = taskOptional.get();
+            task.setId(theTask.getId());
+            task.setTaskCreator(theTask.getTaskCreator());
+            task.setCreatedAt(theTask.getCreatedAt());
+            if (task.getTaskStatus().equals(Status.done)) {
+                task.setCompletedAt(LocalDateTime.now());
             }
-
+        } else {
+            throw new ResourceNotFoundException("Task not found");
         }
         return taskRepository.save(task);
     }
 
     @Override
     public List<Task> getByStatus(Status status) {
-        return taskRepository.findAllByTaskStatus(status);
+        return taskRepository.findByTaskStatusAndTaskCreatorId(status, authenticationFacade.getId());
     }
 }
