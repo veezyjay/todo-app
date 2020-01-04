bf5dc379f2b142ac7b638f3423029670fe305808
Victor Bassey <viktorbasi545@gmail.com>
2020-01-04 15:30:49 +0100

    Include methods to get tasks by specific users

--- src/main/java/com/veezy/todoapp/repository/TaskRepository.java
+++ src/main/java/com/veezy/todoapp/repository/TaskRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    List<Task> findAllByTaskStatus(Status taskStatus);
+    List<Task> findByTaskStatusAndTaskCreatorId(Status taskStatus, Integer creatorId);
+    Optional<Task> findByIdAndTaskCreatorId(Integer taskId, Integer creatorId);
 }
